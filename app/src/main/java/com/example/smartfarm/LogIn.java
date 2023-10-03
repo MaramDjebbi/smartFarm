745a1cb9cce04e6b43edbfadfeb15605977abfe9
MaramDjebbi <djebbi.maram@etudiant-isi.utm.tn>
2023-10-03 21:32:18 +0100

    implemented sign in

--- app/src/main/java/com/example/smartfarm/LogIn.java
+++ app/src/main/java/com/example/smartfarm/LogIn.java
@@ -1,8 +1,11 @@
 package com.example.smartfarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,32 +13,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
 
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        TextView username = (TextView) findViewById(R.id.username);
+        auth = FirebaseAuth.getInstance();
+        TextView email = (TextView) findViewById(R.id.email);
         TextView password = (TextView) findViewById(R.id.password);
         MaterialButton login = (MaterialButton) findViewById(R.id.login);
+        TextView signup = (TextView) findViewById(R.id.signup);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
-                    Toast.makeText(LogIn.this, "SUCCESS", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(LogIn.this, "FAILURE !", Toast.LENGTH_SHORT).show();
+                String eMail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if(!eMail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(eMail).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(eMail, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LogIn.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LogIn.this, dashBoard.class));
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LogIn.this, "FAILURE !", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else
+                        password.setError("Password cannot be empty !");
+                }
+                    else if(eMail.isEmpty())
+                        email.setError("Email cannot be empty !");
+                    else
+                        email.setError("Please enter valid email !");
             }
         });
 
-
-
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LogIn.this, SignUp.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/smartfarm/dashBoard.java
@@ -0,0 +1,4 @@
+package com.example.smartfarm;
+
+public class dashBoard {
+}
