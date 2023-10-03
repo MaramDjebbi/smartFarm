4d2b9e5caaa8344bbb336f923beff337b2973ead
MaramDjebbi <djebbi.maram@etudiant-isi.utm.tn>
2023-10-03 20:13:33 +0100

    implemented sign up

--- app/src/main/java/com/example/smartfarm/LogIn.java
+++ app/src/main/java/com/example/smartfarm/LogIn.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +24,18 @@ public class MainActivity extends AppCompatActivity {
         TextView password = (TextView) findViewById(R.id.password);
         MaterialButton login = (MaterialButton) findViewById(R.id.login);
 
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
-                    Toast.makeText(MainActivity.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LogIn.this, "SUCCESS", Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(MainActivity.this, "FAILURE !", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LogIn.this, "FAILURE !", Toast.LENGTH_SHORT).show();
             }
         });
+
+
 
     }
 }
